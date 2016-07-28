75ffc6321d128586f40bbd8b4032d040aa4b52fa
boris-il-forte <boris.ilpossente@hotmail.it>
2016-07-28 14:16:40 +0200

    Added RTT planner plugin stub

---
+++ rtt_planning/include/RTTPlanner.h
@@ -0,0 +1,40 @@
+/*
+ * RTTPlanner.h
+ *
+ *  Created on: 28 lug 2016
+ *      Author: dave
+ */
+
+#ifndef INCLUDE_RTTPLANNER_H_
+#define INCLUDE_RTTPLANNER_H_
+
+/** include the libraries you need in your planner here */
+/** for global path planner interface */
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+
+
+namespace rtt_planning
+{
+
+class RTTPlanner : public nav_core::BaseGlobalPlanner
+{
+public:
+
+    RTTPlanner();
+    RTTPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+    /** overridden classes from interface nav_core::BaseGlobalPlanner **/
+    void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+    bool makePlan(const geometry_msgs::PoseStamped& start,
+                  const geometry_msgs::PoseStamped& goal,
+                  std::vector<geometry_msgs::PoseStamped>& plan);
+};
+};
+
+
+
+#endif /* INCLUDE_RTTPLANNER_H_ */
---
+++ rtt_planning/src/RTTPlanner.cpp
@@ -0,0 +1,74 @@
+/*
+ * rtt_planning,
+ *
+ *
+ * Copyright (C) 2016 Davide Tateo
+ * Versione 1.0
+ *
+ * This file is part of rtt_planning.
+ *
+ * rtt_planning is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rtt_planning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rtt_planning.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <pluginlib/class_list_macros.h>
+#include "RTTPlanner.h"
+
+//register this planner as a BaseGlobalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(rtt_planning::RTTPlanner, nav_core::BaseGlobalPlanner)
+
+using namespace std;
+
+//Default Constructor
+namespace rtt_planning
+{
+
+RTTPlanner::RTTPlanner ()
+{
+
+}
+
+RTTPlanner::RTTPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+{
+    initialize(name, costmap_ros);
+}
+
+
+void RTTPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+{
+
+}
+
+bool RTTPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,  std::vector<geometry_msgs::PoseStamped>& plan )
+{
+
+    plan.push_back(start);
+    for (int i=0; i<20; i++)
+    {
+        geometry_msgs::PoseStamped new_goal = goal;
+        tf::Quaternion goal_quat = tf::createQuaternionFromYaw(1.54);
+
+        new_goal.pose.position.x = -2.5+(0.05*i);
+        new_goal.pose.position.y = -3.5+(0.05*i);
+
+        new_goal.pose.orientation.x = goal_quat.x();
+        new_goal.pose.orientation.y = goal_quat.y();
+        new_goal.pose.orientation.z = goal_quat.z();
+        new_goal.pose.orientation.w = goal_quat.w();
+
+        plan.push_back(new_goal);
+    }
+    plan.push_back(goal);
+    return true;
+}
+};
