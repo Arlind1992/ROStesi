df838c3b40fa2e86b867dfcf2ba53030a4f704cc
Boris_il_forte <davide.tateo90@gmail.com>
2016-08-21 20:59:23 +0200

    Fixed another compilation issue and added stub
    - fixed missing catkin package compilation
    - added stub for theta star planner

---
+++ rtt_planning/include/rtt_planning/ThetaStarPlanner.h
@@ -0,0 +1,55 @@
+/*
+ * rtt_planning,
+ *
+ *
+ * Copyright (C) 2016 Davide Tateo
+ * Versione 1.0
+ *
+ * This file is part of rtt_planning.
+ *
+ * rtt_planning is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rtt_planning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rtt_planning.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_RTT_PLANNING_THETASTARPLANNER_H_
+#define INCLUDE_RTT_PLANNING_THETASTARPLANNER_H_
+
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include <Eigen/Dense>
+
+
+namespace rtt_planning
+{
+
+class ThetaStarPlanner : public nav_core::BaseGlobalPlanner
+{
+public:
+
+	ThetaStarPlanner();
+	ThetaStarPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+    void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros) override;
+    bool makePlan(const geometry_msgs::PoseStamped& start,
+                  const geometry_msgs::PoseStamped& goal,
+                  std::vector<geometry_msgs::PoseStamped>& plan) override;
+
+};
+
+}
+
+#endif /* INCLUDE_RTT_PLANNING_THETASTARPLANNER_H_ */
---
+++ rtt_planning/src/ThetaStarPlanner.cpp
@@ -0,0 +1,68 @@
+/*
+ * rtt_planning,
+ *
+ *
+ * Copyright (C) 2016 Davide Tateo
+ * Versione 1.0
+ *
+ * This file is part of rtt_planning.
+ *
+ * rtt_planning is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rtt_planning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rtt_planning.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <pluginlib/class_list_macros.h>
+#include "rtt_planning/ThetaStarPlanner.h"
+
+using namespace Eigen;
+
+//register this planner as a BaseGlobalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(rtt_planning::ThetaStarPlanner, nav_core::BaseGlobalPlanner)
+
+using namespace std;
+
+//Default Constructor
+namespace rtt_planning
+{
+
+ThetaStarPlanner::ThetaStarPlanner ()
+{
+
+}
+
+ThetaStarPlanner::ThetaStarPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+{
+
+}
+
+
+void ThetaStarPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
+{
+
+}
+
+bool ThetaStarPlanner::makePlan(const geometry_msgs::PoseStamped& start,
+                          const geometry_msgs::PoseStamped& goal,
+                          std::vector<geometry_msgs::PoseStamped>& plan)
+{
+    return false;
+
+}
+
+
+
+};
+
+
+
+
